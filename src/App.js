d5fb8d2ee40a3148e9800a081f382c9074d7413b
Mahmoud Salman <salmanmahmoud70@gmail.com>
2022-07-24 11:06:28 +0200

    About section has been completed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import 'locomotive-scroll/dist/locomotive-scroll.css'
 import { useRef } from 'react'
 import Home from './sections/Home'
 import { AnimatePresence } from 'framer-motion'
+import About from './sections/About'
 
 function App() {
   const containerRef = useRef(null)
@@ -31,6 +32,7 @@ function App() {
           <AnimatePresence>
             <main data-scroll-container ref={containerRef}>
               <Home />
+              <About />
             </main>
           </AnimatePresence>
         </LocomotiveScrollProvider>
